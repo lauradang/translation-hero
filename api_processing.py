5a7a54722870e17a2b3e04766e98aff8761e3cf6
Laura Dang <lauradang@Lauras-MacBook-Air-3.local>
2019-08-04 18:18:15 -0400

    :lipstick: Updating file upload style

--- api_processing.py
+++ api_processing.py
@@ -59,5 +59,3 @@ def detect_image(user_language):
     }
 
     return final_dict
-
-
--- app.py
+++ app.py
@@ -5,18 +5,40 @@ import io
 from api_processing import detect_image
 from languagedictionary import languages
 
+
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+IMAGE_FOLDER = os.path.join('static', 'images')
+STYLE_FOLDER = os.path.join('static', 'style')
+
+app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
+app.config['STYLE_FOLDER'] = STYLE_FOLDER
+
 @app.route("/")
 def home():
-    return render_template("uploadPage.html", languages=languages)
+    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'world.png')
+    style = os.path.join(app.config['STYLE_FOLDER'], 'style.css')
+    script = os.path.join(app.config['STYLE_FOLDER'], 'script.js')
+    return render_template (
+        "uploadPage.html", 
+        languages=languages, 
+        logo=full_filename, 
+        style=style,
+        script=script
+    )
+
+@app.route("/test")
+def test():
+    return render_template("index.html")
 
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
     target = os.path.join(APP_ROOT, "images/")
-
+    print(target)
+    print(request.files.getlist("file"))
+    print("JDKLSAJFDKSALJFDKSALJFDKSALJFKDSALJFDKSALJ")
     if not os.path.isdir(target):
         os.mkdir(target)
     
@@ -26,7 +48,9 @@ def upload():
         destination = "/".join([target, filename])
 
         upload.save(destination)
-        user_language = request.form.get("userLanguage", None)
+        # user_language = request.form.get("userLanguage")
+        user_language = request.form.get("userLanguage")
+        print(user_language)
 
         obj_name_english = detect_image(user_language)['obj_name']
         obj_name_user_lang = detect_image(user_language)['translation']
@@ -35,7 +59,6 @@ def upload():
 
     return render_template (
         "process.html", 
-        image_name=filename, 
         obj_name_english=obj_name_english, 
         obj_name_user_lang=obj_name_user_lang,
         user_language=user_language
