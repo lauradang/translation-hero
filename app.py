0b39a4553c89d6d3707a45e169c1bf6fa74edf77
Laura Dang <lauradang@Lauras-MacBook-Air-3.local>
2019-08-04 20:27:12 -0400

    :bug: Fixing uploading image bug

--- app.py
+++ app.py
@@ -16,13 +16,16 @@ STYLE_FOLDER = os.path.join('static', 'style')
 app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
 app.config['STYLE_FOLDER'] = STYLE_FOLDER
 
+full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'world.png')
+style = os.path.join(app.config['STYLE_FOLDER'], 'style.css')
+script = os.path.join(app.config['STYLE_FOLDER'], 'script.js')
+
 @app.route("/")
 def home():
-    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'world.png')
-    style = os.path.join(app.config['STYLE_FOLDER'], 'style.css')
-    script = os.path.join(app.config['STYLE_FOLDER'], 'script.js')
+    uploaded = False
     return render_template (
         "uploadPage.html", 
+        uploaded=uploaded,
         languages=languages, 
         logo=full_filename, 
         style=style,
@@ -35,33 +38,35 @@ def test():
 
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
-    target = os.path.join(APP_ROOT, "images/")
-    print(target)
-    print(request.files.getlist("file"))
-    print("JDKLSAJFDKSALJFDKSALJFDKSALJFKDSALJFDKSALJ")
+    target = os.path.join(APP_ROOT, "images")
+
     if not os.path.isdir(target):
         os.mkdir(target)
-    
+
     for upload in request.files.getlist("file"):
         filename = upload.filename
-        ext = os.path.splitext(filename)[1]
         destination = "/".join([target, filename])
 
         upload.save(destination)
-        # user_language = request.form.get("userLanguage")
         user_language = request.form.get("userLanguage")
-        print(user_language)
 
         obj_name_english = detect_image(user_language)['obj_name']
         obj_name_user_lang = detect_image(user_language)['translation']
 
         os.remove(destination) 
 
+    uploaded = True
+
     return render_template (
-        "process.html", 
+        "uploadPage.html", 
+        uploaded=uploaded,
         obj_name_english=obj_name_english, 
         obj_name_user_lang=obj_name_user_lang,
-        user_language=user_language
+        user_language=user_language,
+        languages=languages, 
+        logo=full_filename, 
+        style=style,
+        script=script
     )
 
 if __name__ == '__main__':
